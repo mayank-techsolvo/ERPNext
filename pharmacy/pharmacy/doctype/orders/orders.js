fc829e347b308a105391f90268e22f277c32eb15
mayank-techsolvo <mayank.raghuvanshi@techsolvo.com>
2024-06-06 15:31:03 +0530

    all doctype added

---
+++ pharmacy/pharmacy/doctype/orders/orders.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Smile and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Orders", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ pharmacy/pharmacy/doctype/stock_transaction/test_stock_transaction.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Smile and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestStockTransaction(FrappeTestCase):
+	pass
