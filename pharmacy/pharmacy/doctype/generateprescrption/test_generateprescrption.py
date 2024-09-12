d568f63c500e3e44f9988d8b160363e7ba83c5ac
mayank-techsolvo <mayank.raghuvanshi@techsolvo.com>
2024-09-12 08:27:50 +0000

    server changes

---
+++ pharmacy/pharmacy/doctype/generateprescrption/test_generateprescrption.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Smile and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestGeneratePrescrption(FrappeTestCase):
+	pass
